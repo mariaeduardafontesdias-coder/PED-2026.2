4edece4b41d1f75f989125d1d0aeb7b1a2ed59e4
mariaeduardafontesdias-coder <mariaeduardafontesdias@gmail.com>
2026-08-10 20:21:09 -0300

    Add files via upload

---
+++ lista-01/Atividade 01/Operações Básicas - Soma de dois números/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2, soma;
+
+    scanf("%d", &numero1);
+    scanf("%d", &numero2);
+
+    soma = numero1 + numero2;
+
+    printf("%d\n", soma);
+
+    return 0;
+}
---
+++ lista-01/Atividade 01/Operações Básicas - área do circulo/source code
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float raio, area;
+
+    scanf("%f", &raio);
+
+    area = M_PI * raio * raio;
+
+    printf("%.2f\n", area);
+
+    return 0;
+}
